de6df93d1a193a084acfd22ef92972b07d52ceb5
CharlesBZ <charles.a.bestjr@gmail.com>
2025-04-17 11:27:24 -0400

    B: add ProductController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController
+class HomeController extends AbstractController
 { 
     #[Route('/')]
     public function index(): Response
-    {
-        return new Response("Hello from a home controller");
+    {   
+        return $this->render('home/index.html.twig');
     }
 }
+
+/*
+$contents = $this->renderView('home/index.html.twig', [
+  'title' => 'Home',
+  'message' => 'Hello from a home controller'
+]);
+
+return new Response($contents);
+*/
